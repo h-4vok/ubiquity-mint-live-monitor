d218ebdeff675d132070cc36f4cf9dcb9e815ddf
German Dario <germangp088@gmail.com>
2022-07-05 20:10:13 -0300

    Home mock components

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import { PureComponent } from "react";
-import logo from '../../../logo.svg';
-import './Home.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import AddToQueueIcon from '@mui/icons-material/AddToQueue';
+import Stack from '@mui/material/Stack';
+//import logo from '../../../logo.svg';
 import { Monitor } from "../../../lib/monitor";
+import './Home.css';
 
 export class Home extends PureComponent {
 
-  async componentDidMount() {
+  async startMonitoring() {
     console.log(process.env)
     const monitor = new Monitor(process.env.REACT_APP_API_KEY, process.env.REACT_APP_BLOCK_NUMBER);
     await monitor.start();
@@ -15,18 +19,31 @@ export class Home extends PureComponent {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {
+            //<img src={logo} className="App-logo" alt="logo" />
+          }
+          <h1>Ubiquity</h1>
+          <h2>Live Mint Monitor</h2>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Monitor all NFT mints in the Solanas blockchain
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Stack direction="row" spacing={2}>
+            <TextField
+              id="outlined-number"
+              label="Start with block"
+              type="number"
+              placeholder="Latest"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <Button
+              variant="contained"
+              endIcon={<AddToQueueIcon />}
+              onClick={() => this.startMonitoring()}>
+              GO!
+            </Button>
+          </Stack>
         </header>
       </div>
     );
