643b7eafe7bd9df3c0d704fa1afcd8a22edccc8c
Guzmán <christian.guzman@productmadness.com>
2022-07-07 17:37:34 +0100

    ULMM-6 ULMM-7

--- src/components/molecules/FungibleTokenRow.jsx
+++ src/components/molecules/FungibleTokenRow.jsx
@@ -1,14 +1,21 @@
 import { Paper, Stack, Container } from "@mui/material";
-import { FungibleTokenThumbnail, Title, Label } from "../atoms";
+import { FungibleTokenThumbnail, Label } from "../atoms";
 
 export const FungibleTokenRow = (props) => (
-  <Paper className="padding--40px hover-animation">
-    <Stack direction="row" spacing={2}>
-      <FungibleTokenThumbnail imageSrc="https://www.arweave.net/oiY7orCWNjVt8HkSgZKt2zql7LcsOpoo5W8W_fjKmIg?ext=png" />
-      <Container className="text-align--left">
-        <Label variant="h4">NFT name</Label>
-        <Label variant="span">NFT uri</Label>
-      </Container>
-    </Stack>
-  </Paper>
+  <div onClick={() => goToNftDetail({}, props.onOpenNftDetail)}>
+    <Paper className="padding--40px hover-animation">
+      <Stack direction="row" spacing={2}>
+        <FungibleTokenThumbnail imageSrc="https://www.arweave.net/oiY7orCWNjVt8HkSgZKt2zql7LcsOpoo5W8W_fjKmIg?ext=png" />
+        <Container className="text-align--left">
+          <Label variant="h4">NFT name</Label>
+          <Label variant="span">NFT uri</Label>
+        </Container>
+      </Stack>
+    </Paper>
+  </div>
 );
+
+const goToNftDetail = (nftData, onOpenNftDetail) => {
+  // we will show a popup of the nft data
+  onOpenNftDetail({});
+};
--- src/components/pages/LiveMonitorPage.jsx
+++ src/components/pages/LiveMonitorPage.jsx
@@ -10,24 +10,40 @@
 //   await monitor.start();
 // };
 import { Grid, Stack, Container, Box } from "@mui/material";
+import { useState } from "react";
 import { Title, Label } from "../atoms";
-import { FungibleTokenRow } from "../molecules";
+import { FungibleTokenRow, BasicModal } from "../molecules";
 
-export const LiveMonitorPage = () => (
-  <Box>
-    <Grid container className="text-align--left" spacing={2}>
-      <Grid item xs={12}>
-        <Title variant="h5">Ubiquity Live Mint Monitor</Title>
+export const LiveMonitorPage = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedNft, setSelectedNft] = useState(null);
+
+  const openNftDetail = (nftData) => {
+    setSelectedNft(nftData);
+    setOpenModal(true);
+  };
+
+  return (
+    <Box>
+      <Grid container className="text-align--left" spacing={2}>
+        <Grid item xs={12}>
+          <Title variant="h5">Ubiquity Live Mint Monitor</Title>
+        </Grid>
+        <Grid item xs={12}>
+          <Label>Exploring... (current / latest)</Label>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Label>Exploring... (current / latest)</Label>
-      </Grid>
-    </Grid>
-    <Stack spacing={2} className="margin-top--20px">
-      <FungibleTokenRow />
-      <FungibleTokenRow />
-      <FungibleTokenRow />
-      <FungibleTokenRow />
-    </Stack>
-  </Box>
-);
+      <Stack spacing={2} className="margin-top--20px">
+        <FungibleTokenRow onOpenNftDetail={openNftDetail} />
+        <FungibleTokenRow onOpenNftDetail={openNftDetail} />
+        <FungibleTokenRow onOpenNftDetail={openNftDetail} />
+        <FungibleTokenRow onOpenNftDetail={openNftDetail} />
+      </Stack>
+      <BasicModal
+        open={openModal}
+        setOpen={setOpenModal}
+        nftData={selectedNft}
+      />
+    </Box>
+  );
+};
