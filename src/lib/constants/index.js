cf0b9c719e511cafddb6663b8bc66387d993ccc6
German Dario <germangp088@gmail.com>
2022-07-12 21:36:50 -0300

    Link to solscan

--- src/lib/constants/index.js
+++ src/lib/constants/index.js
@@ -5,5 +5,5 @@ export {
 } from "./pages";
 
 export const solscanApi = 'https://api.solscan.io/account?address='
-
+export const solscanUrl = 'https://solscan.io/token/'
 export const lineBreakCode = '%0A'
